7be4500e131eeba5e3a83a7c8a94bd0e61380fbc
jan komarek <cikanuwu@gmail.com>
2024-04-16 12:31:58 +0200

    ghjk

---
+++ nakupnikosik.py
@@ -0,0 +1,7 @@
+print ("Vítejte na webové stránce oblock zde můžete zakoupit pc komponenty")
+
+zbozi = ["RYZEN 7 800x3d", "NVIDIA 4070ti", "CORSAIR Vengeance RGB DDR4 32GB", "GIGABYTE zdroj UD750GM, 750W", "Asus ROG MAXIMUS Z790 HERO"]
+kosik = []
+
+
+
